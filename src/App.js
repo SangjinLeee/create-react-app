e655ff0f532bee0a3aee22d28a85e111b3ddd02e
SangjinLeee <122560041+SangjinLeee@users.noreply.github.com>
2023-02-13 18:38:29 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ function App() {
   return (
 
 <>
-    <div class="outline">
-      <div class="lecard">
-        <h2>Some Nerd</h2>
-        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
-      </div>
-    </div>
-  <div class="buttons">
-  
-  <button class="copyCat">Copy</button>
-  <button id="del">Delete</button>
-  <button class="btnDet">Toggle Details</button> 
-  <button class="btnCT">Change Title</button>
-  <button  class="btnBG">Change Background Color</button>
-  
-  </div>
+<button id="duplicate" class="button">Duplicate</button>
+
+<button id="toggleBackGroundColor" class="button">Change Background Color</button>
+
+<button id="changeTitle" class="button">Change Title</button>
+
+<button id="deleteButton" class="button">Delete</button>
+
+<section class="card">
+  <header class="name">
+    <h1 id="title" class="title"> Coffee </h1>
+    </header>
+  <p>
+    "Good morning, I like coffeeeeee" </p>
+  <section class="content">
+    <img src="https://thumbor.bigedition.com/husky-good-morning-meme/pH3EqF8t0ug2q1rb8xQUIfy_TIM=/800x739/filters:format(webp):quality(80)/granite-web-prod/48/f0/48f096f394fe421fa43128fa65a1f2a1.jpeg"
+alt="https://www.workandmoney.com/s/good-morning-memes-19ec5c5d8885431a" width=200px>
+    <button id = "detailBtn" class="detailsButton">Detail </button>
+</section>
+</section>
   
 </>
   );
